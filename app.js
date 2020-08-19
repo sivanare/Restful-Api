c2fcd6e77f1a6fd8454d66828ee62065172b00df
unknown <sivanaray@gmail.com>
2020-08-19 17:41:05 +0200

    creating mongoose database and connect

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 
+// free mongo database https://mlab.com/
+
+// Middleware
+
+// app.use('/posts', () => {
+//   console.log('From middleware');
+// });
+
 //ROUTES
 app.get('/', (req, res) => {
   res.send('we are on home');
+});
+app.get('/posts', (req, res) => {
+  res.send('we are on posts');
 });
 
 // to listen to the server

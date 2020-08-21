8cda3386443dd0d9bd4d4fc272d4a5a924c5c8a8
unknown <sivanaray@gmail.com>
2020-08-21 17:50:25 +0200

    creating routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Getting all
+router.get('/', (req, res) => {});
+
+// Getting one
+router.get('/:id', (req, res) => {});
+
+// Creating one
+router.post('/', (req, res) => {});
+
+// updating one
+router.patch('/:id', (req, res) => {});
+
+// deleting one
+router.delete('/:id', (req, res) => {});
+
 module.exports = router;

b944adab7f44715cce9b610e5205512be0b9ff41
unknown <sivanaray@gmail.com>
2020-08-21 14:42:01 +0200

    update

--- routes/about.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.send('we are on about');
-});
-router.get('/specific', (req, res) => {
-  res.send('we are on specific');
-});
-module.exports = router;

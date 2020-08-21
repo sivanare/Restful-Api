3b017393c7191303aa0d1203b750a74a3e6c61ca
unknown <sivanaray@gmail.com>
2020-08-21 19:21:56 +0200

    Getting all router

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Subscriber = require('../models/subscriber');
 
 // Getting all
-router.get('/', (req, res) => {
-  res.send('hello from get');
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Getting one

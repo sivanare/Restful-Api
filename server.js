01952224ec254c15ba33100a0ded94b1c6ae0c46
unknown <sivanaray@gmail.com>
2020-08-21 17:40:02 +0200

    connect routes using middleware

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv').config();
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 mongoose
-  .connect('mongodb://localhost/subscribers', { useNewUrlParser: true })
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connected to database'));
+
+app.use(express.json());
+
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
 
 app.listen(3000, () => {
   console.log('server has started');
